11d3b085934e779f64128a9340451b9eb543325c
Elena <ebe2017@yandex.ru>
2024-11-07 21:36:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onClickDecrease = () => {
+    if (count > -50) {
+      setCount(prev => prev - 1);
+    }
+  }
+
+  const onClickReset = () => {
+    setCount(0);
+  }
+
+  const onClickIncrease = () => {
+    if (count < 50) {
+      setCount(prev => prev + 1);
+    }
+  }
+
+  const textColor = count < 0 ? 'rgb(216, 40, 40)' : count > 0 ? 'rgb(31, 176, 31)' : 'rgb(152, 146, 146)';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Counter</h1>
+        <p style={{color: textColor}}>{count}</p>
+        <div className="buttons">
+          <button onClick={onClickDecrease} className="btn-decr">Decrease</button>
+          <button onClick={onClickReset} className="btn-res">Reset</button>
+          <button onClick={onClickIncrease} className="btn-incr">Increase</button>
+        </div>
+      </div>
     </div>
   );
 }
